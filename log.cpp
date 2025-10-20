fcac2c01d1aeea5637065c66feab6d87a46a8ca7
ThatzOkay <noduhgamingxd@gmail.com>
2025-10-21 00:07:45 +0200

    Working cli

--- log.cpp
+++ log.cpp
@@ -1,24 +1,52 @@
 #include "log.h"
 #include <cstdarg>
 #include <cstdio>
+
+#include "config.h"
 #include "pico/stdio.h"
 #include "tusb.h"
-#include "config.h"
 
-void log(const char *format, ...)
+void debug_log(const char* format, ...)
 {
-#if ENABLE_DEBUG
-    uint8_t out_buffer[MAX_PACKET] = {};
+    if (led_cfg->debug.enable)
+    {
+        char out_buffer[256] = {};
+
+        va_list args;
+        va_start(args, format);
+        const int len = vsnprintf(out_buffer, sizeof(out_buffer), format, args);
+        va_end(args);
+
+        if (len > 0)
+        {
+            tud_cdc_n_write(2, out_buffer, len < sizeof(out_buffer) ? len : sizeof(out_buffer));
+            tud_cdc_n_write_flush(2);
+        }
+    }
+}
+
+void cli_log(const char* format, ...)
+{
+    char out_buffer[MAX_PACKET] = {};
 
     va_list args;
     va_start(args, format);
-    int len = vsnprintf((char*)out_buffer, sizeof(out_buffer), format, args);
+    int len = vsnprintf(out_buffer, sizeof(out_buffer), format, args);
     va_end(args);
 
-    if (len > 0)
+    if (len <= 0) return;
+
+    int written = 0;
+    while (written < len)
     {
-        tud_cdc_n_write(2, out_buffer, (len < sizeof(out_buffer)) ? len : sizeof(out_buffer));
-        tud_cdc_n_write_flush(2);
+        int avail = tud_cdc_n_write_available(2);
+        if (avail > 0)
+        {
+            int to_write = (len - written < avail) ? (len - written) : avail;
+            tud_cdc_n_write(2, &out_buffer[written], to_write);
+            tud_cdc_n_write_flush(2);
+            written += to_write;
+        }
+        tud_task();
     }
-#endif
 }
--- log.h
+++ log.h
@@ -1,1 +1,2 @@
-void log(const char * format, ...);
+void debug_log(const char * format, ...);
+void cli_log(const char * format, ...);
