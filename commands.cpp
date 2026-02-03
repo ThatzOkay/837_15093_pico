017cee423717b8fa5fbfd99ed04ca6472076b8e8
Kayori <noduhgamingxd@gmail.com>
2026-02-03 23:31:42 +0100

    Fix

--- commands.cpp
+++ commands.cpp
@@ -164,6 +164,7 @@ void log_effect_usage() {
             cli_log("|");
         }
     }
+    cli_log(">\n");
     cli_log("Please dont use fade effect as it is broken on the pico>\n");
     cli_log("Seemd like particles is also broken. It's not saving>\n");
 }
